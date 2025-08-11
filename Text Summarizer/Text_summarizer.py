5122beb938741070c631ecda23d24cc95be46f85
Dhanam-patel <dhanampatel2005@gmail.com>
2025-08-11 19:43:28 +0530

    Commit Sussefully

--- Text Summarizer/Text_summarizer.py
+++ Text Summarizer/Text_summarizer.py
@@ -2,11 +2,11 @@ from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
 from dotenv import load_dotenv
 import streamlit as st
 from langchain_core.prompts import PromptTemplate, load_prompt
-import pyttsx3
+# import pyttsx3
 
 load_dotenv()
 
-engine = pyttsx3.init()
+# engine = pyttsx3.init()
 
 llm = HuggingFaceEndpoint(
     repo_id="baidu/ERNIE-4.5-300B-A47B-Base-PT",
@@ -19,7 +19,7 @@ tone = st.selectbox("Select the desired tone for the summary",["Professional", "
 length = st.selectbox("Select the desired length for the summary(The length of summary will also depend on the length of text)",["short(50 to 60 words)", "Medium (100 to 120 words)", "Large (more than 150 words)"])
 Format = st.selectbox("Select the desired Format for the summary",["Paragraph", "Numbered Steps", "Bullet Points"])
 language = st.selectbox("Select the desired language for the summary",["English", "Hindi", "Gujarati"])
-template = load_prompt("D:/CODE/Python/Langchin/Projects/template.json")
+template = load_prompt("template.json")
 prompt = template.invoke({
     "text": text,
     "tone": tone,
@@ -31,5 +31,5 @@ prompt = template.invoke({
 if st.button("Summarize"):
     result = model.invoke(prompt)
     st.write(result.content)
-    engine.say(result.content)
-    engine.runAndWait()
+    # engine.say(result.content)
+    # engine.runAndWait()
